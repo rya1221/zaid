6822b1fbebeabcfb30ba8347f3369577566e76ff
rya1221 <91537646+rya1221@users.noreply.github.com>
2023-01-05 17:39:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 if os.path.exists("local.env"):
     load_dotenv("local.env")
 que = {}
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "22165592").split()))
 API_ID = int(getenv("API_ID", "6435225"))
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-LOG_CHAT = int(getenv("LOG_CHAT", "777000"))
+API_HASH = getenv("API_HASH", "aa2886eda514d8beaa98695cfe3060ff")
+LOG_CHAT = int(getenv("LOG_CHAT", "687154450"))
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "")
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "")
 SPOTIFY_USERNAME = getenv("SPOTIFY_USERNAME", "")
